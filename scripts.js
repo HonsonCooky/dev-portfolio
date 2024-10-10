b47c136867d4301796595265a134af3dea289d48
Harrison Cook <HonsonCooky@gmail.com>
2024-10-11 00:11:05 +1300

    init

---
+++ scripts.js
@@ -0,0 +1,3 @@
+const profileImg = document.getElementById("profile-img");
+profileImg.onmouseover = () => (profileImg.src = "./assets/imgs/profile.png");
+profileImg.onmouseout = () => (profileImg.src = "./assets/imgs/profile-pixel.png");
