051d92faab81573afd838b3ccf6edd8f9eed75cc
Harrison Cook <HonsonCooky@gmail.com>
2024-10-11 15:04:54 +1300

    Z Energy Quest start

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 const profileImg = document.getElementById("profile-img");
 profileImg.onmouseover = () => (profileImg.src = "./assets/imgs/profile.png");
-profileImg.onmouseout = () => (profileImg.src = "./assets/imgs/profile-pixel.png");
+profileImg.onmouseout = () =>
+  (profileImg.src = "./assets/imgs/profile-pixel.png");
