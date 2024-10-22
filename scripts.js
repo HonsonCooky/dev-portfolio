13031b5ef245864fcdc63142cb0d4411b738c8eb
Harrison Cook <HonsonCooky@gmail.com>
2024-10-22 22:08:17 +1300

    update style

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
 const imgs = document.querySelectorAll("img");
-function imgMouseOver() {
-    this.src = this.getAttribute("hover");
-}
-function imgMouseOut() {
-    this.src = this.getAttribute("default");
-}
 imgs.forEach((img) => {
     if (!img.src) {
         img.src = img.getAttribute("default");
-        img.onmouseover = imgMouseOver;
-        img.onmouseout = imgMouseOut;
+        img.onmouseover = function () {
+            this.src = this.getAttribute("hover");
+        };
+        img.onmouseout = function () {
+            this.src = this.getAttribute("default");
+        };
     }
 
     img.onclick = function () {
