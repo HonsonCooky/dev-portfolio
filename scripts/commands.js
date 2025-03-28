0fe9c0b8342af3c66c86b631a12b3b3f4cd933a8
Harrison Cook <harrison.cook1@xero.com>
2025-03-28 15:21:02 +1300

    switch

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,6 +1,16 @@
+async function printFile(fileName) {
+	const fileContent = await (await fetch(`../pages/${fileName}`)).text();
+	if(!fileContent) throw Error(`Unable to find file ${fileName}`);
+	return function () {
+		const fileDiv = document.createElement("div");
+		fileDiv.innerHTML = fileContent;
+		
+	};
+}
+
 const commands = {
 	whoami: {
-		eval: function (_) {},
+		eval: printFile()
 	},
 };
 
