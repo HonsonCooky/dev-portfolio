4ab1cd594f153fffb5330acad8bccea1b46e3520
Harrison Cook <HonsonCooky@gmail.com>
2025-04-13 13:34:38 +1200

    Init button components

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import {RouterLink, RouterView} from 'vue-router'
 
 <template>
   <header>
+    <h1>C:\HarrisonCook&gt;</h1>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
