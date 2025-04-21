719adbc9af3d47cd9c3677df10be40c5535dfb9a
Harrison Cook <HonsonCooky@gmail.com>
2025-04-22 10:22:07 +1200

    update to Nuxt

---
+++ README.md
@@ -0,0 +1,6 @@
+# Developer Portfolio
+
+Welcome, traveling developers! This is my online portfolio; An online display of my work and observations, built 
+with Nuxt.
+
+You'll find the outcome of this directory at [`https://about.honsoncooky.dev`](https://about.honsoncooky.dev).
---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (pageTitle) => {
+    return pageTitle ? `Harrison Cook | ${pageTitle}` : 'Harrison Cook'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <nuxt-route-announcer />
+    <app-header/>
+    <nuxt-page/>
+    <app-footer />
+  </div>
+</template>
