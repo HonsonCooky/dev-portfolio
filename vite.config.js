183e1bbf1b5e38229073327dff12468754924fa6
Harrison Cook <HonsonCooky@gmail.com>
2025-05-04 18:18:51 +1200

    Add dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/dev-portfolio/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
