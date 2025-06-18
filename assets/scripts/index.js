aa6960139fa637e0a1139962763858e4cc6f3e25
Harrison Cook <HonsonCooky@gmail.com>
2025-06-19 06:04:07 +1200

    init styles and js

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+/**
+ * Assumes deferred module is used.
+ */
+
+// Working Experience
+const start = new Date('2021/08/01') // Account for 3 months at Raygun
+const diff = (new Date() - start) / (1000 * 60 * 60 * 24 * 365)
+const yearsWorkingElement = document.getElementById('years-working')
+if (yearsWorkingElement) yearsWorkingElement.innerText = diff.toFixed(2)
